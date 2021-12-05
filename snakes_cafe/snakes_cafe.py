1df4611b7961f9cb240e5a4e994bc157dcc4eb3b
Clarissa P <clarissapamonicutt@gmail.com>
2021-12-04 23:56:58 -0800

    first push, incomplete

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,37 +1,56 @@
-$ python snakes_cafe.py
-**************************************
-**    Welcome to the Snakes Cafe!   **
-**    Please see our menu below.    **
-**
-** To quit at any time, type "quit" **
-**************************************
+from typing import Counter
 
-Appetizers
-----------
-Wings
-Cookies
-Spring Rolls
 
-Entrees
--------
-Salmon
-Steak
-Meat Tornado
-A Literal Garden
+def opening():
+  menu = """
+  **************************************
+  **    Welcome to the Snakes Cafe!   **
+  **    Please see our menu below.    **
+  **
+  ** To quit at any time, type "quit" **
+  **************************************
 
-Desserts
---------
-Ice Cream
-Cake
-Pie
+  Appetizers
+  ----------
+  Wings
+  Cookies
+  Spring Rolls
 
-Drinks
-------
-Coffee
-Tea
-Unicorn Tears
+  Entrees
+  -------
+  Salmon
+  Steak
+  Meat Tornado
+  A Literal Garden
 
-***********************************
-** What would you like to order? **
-***********************************
->
+  Desserts
+  --------
+  Ice Cream
+  Cake
+  Pie
+
+  Drinks
+  ------
+  Coffee
+  Tea
+  Unicorn Tears
+
+  ***********************************
+  ** What would you like to order? **
+  ***********************************
+  """
+  print(menu)
+
+
+if __name__ == "__main__":
+  opening()
+
+  Order typed into terminal(any words)
+  Count
+  empty list for orders
+
+  while loop not equal to quit
+  add logic for orders added with their counts
+
+  order needs to go into new list
+  count needs to be changed 
